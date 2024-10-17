167906393873930cc27481097ec026cfd0e789ed
Hayato222 <g2227070@st.t-kougei.ac.jp>
2024-10-17 14:50:21 +0900

    add script

---
+++ Zemi-Production/Assets/Scripts/DestroyArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Oculus.Interaction
+{
+    public class DestroyArea : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("AtttackItems"))
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
