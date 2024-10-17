957990d1a702afa7576343f878e71f8a0e321478
Hayato222 <g2227070@st.t-kougei.ac.jp>
2024-10-17 12:20:28 +0900

    prefab追加

---
+++ Zemi-Production/Assets/Scripts/RespownItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Oculus.Interaction
+{
+    public class RespownItem : MonoBehaviour
+    {
+        private Vector3 respownPoint;
+        private void Start()
+        {
+            this.transform.position = respownPoint;
+        }
+
+        private void Respown()
+        {
+            
+        }
+    }
+}
